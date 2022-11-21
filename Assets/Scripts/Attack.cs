0b26b34cc2cd6b5987d749357c2bd39a93f5e3d7
EleanorOfBingen <eleanor.ahlberg@edu.futuregames.nu>
2022-11-21 17:08:36 +0100

    WorkedOnFixingSomepartsOfAttackingAndMovement

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -9,21 +9,29 @@ public class Attack : MonoBehaviour
     [SerializeField]private SwordBox sb;
     [SerializeField]private float howLongAttack = 0.5f;
 
-    [Header ("Attack Stats")] 
+    [Header ("Sword regain stat")] 
     [SerializeField]private float attackMeter = 100;
     [SerializeField]private float attackDecreesment = 10;
     [SerializeField]private float attackIncreasementRate;
+
+    [Header("Sword Push on Character")]
+    [SerializeField] private float basicAttackPushForward = 20;
 
     private float attackMeterMin = 0;
     private float attackMeterMax;
     private float attackTimer;
     private bool attacking;
 
+
+
+    private Movement movement;
+
+
     // Start is called before the first frame update
     void Start()
     {
         attackMeterMax = attackMeter;
-        
+        movement = GetComponent<Movement>();
     }
 
     // Update is called once per frame
@@ -34,8 +42,8 @@ public class Attack : MonoBehaviour
         if (attacking)
         {
             attackTimer -= Time.deltaTime;
-            
-            if(attackTimer < 0)
+           
+            if (attackTimer < 0)
             {
                 attacking = false;
 
@@ -56,12 +64,12 @@ public class Attack : MonoBehaviour
     }
     private void OnSword()
     {
-       
-
         sb.ActivateAttack(howLongAttack);
         attackMeter -= attackDecreesment;
         attackTimer = howLongAttack;
+        movement.SwordAttack(howLongAttack, basicAttackPushForward);
         attacking = true;
+       
 
     }
     public void AttackMeterIncrease(float increasement)
--- Assets/Scripts/SwordBox.cs
+++ Assets/Scripts/SwordBox.cs
@@ -7,8 +7,10 @@ public class SwordBox : MonoBehaviour
 
     bool buttonPressed;
     private float attackTime;
-
+    public List<Collider> col;
     [SerializeField] private Attack attack;
+
+    private float force = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,9 @@ public class SwordBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+
         if (buttonPressed)
         {
             if (attackTime < 0)
@@ -31,21 +36,31 @@ public class SwordBox : MonoBehaviour
 
             BoxCollider bc = GetComponent<BoxCollider>();
 
-            Collider[] col = Physics.OverlapBox(transform.position + (Vector3.forward * 0.3f), bc.size, transform.rotation);
+            //Collider[] col = Physics.OverlapBox(transform.position + (Vector3.forward * 0.3f), bc.size/2, transform.rotation);
+            for (var i = col.Count - 1; i > -1; i--)
+            {
+                if (col[i] == null)
+                    col.RemoveAt(i);
+            }
+
             foreach (Collider c in col)
             {
 
-                if(c.gameObject.tag != "Player")
+                if(c.gameObject.tag != "Player" && c != null)
                 {
 
-                    Destroy(c.gameObject);
+                    Destroy(c.gameObject, 0.5f);
+                    Vector3 pushdirections = (c.gameObject.transform.position - transform.position).normalized;
+
+                    c.gameObject.transform.Translate(pushdirections * force * Time.deltaTime);
                     attack.AttackMeterIncrease(25);
 
                 }
 
             }
+     
 
-
+            col = new List<Collider>();
 
 
         }
@@ -56,18 +71,45 @@ public class SwordBox : MonoBehaviour
     }
     private void OnTriggerStay(Collider other)
     {
+
+        if (col.IndexOf(other) < 0)
+        {
+
+            col.Add(other);
+
+
+        }
         //Debug.Log(other.name);
-        //if (buttonPressed && other.tag != "Player")
-        //{
+        if (buttonPressed && other.tag != "Player")
+        {
            
-        //   Destroy(other.gameObject);
+         
 
 
-        //}
+        }
 
 
 
     }
+    private void OnTriggerExit(Collider other)
+    {
+
+        col.Remove(other);
+        //Debug.Log(other.name);
+        if (buttonPressed && other.tag != "Player")
+        {
+
+          
+
+
+        }
+
+
+
+    }
+
+
+
     public void ActivateAttack(float attackDuration)
     {
         
