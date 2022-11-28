948d68f90e946aa6e88e5055229f6d0beb671f94
HerraHinrik <60097312+HerraHinrik@users.noreply.github.com>
2022-11-28 13:29:03 +0100

    Basic Animation Set up

--- Assets/Scripts/AnimationContoller.cs
+++ Assets/Scripts/AnimationContoller.cs
@@ -5,15 +5,72 @@ using UnityEngine.InputSystem;
 
 public class AnimationContoller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator m_Animator;
+
+    private Vector2 leftStickPosition;
+    private bool bigSwordPressed;
+    private bool swordPressed;
+    
+    
+    private void Update()
     {
         
+        
+
+        if (leftStickPosition != Vector2.zero)
+        {
+            
+            m_Animator.SetTrigger("Walk");
+            m_Animator.ResetTrigger("Idal");
+        }
+        else
+        {
+            
+            m_Animator.SetTrigger("Idal");
+            m_Animator.ResetTrigger("Walk");
+        }
+      
+        
+    }
+    private void OnMove(InputValue movementValue)
+    {
+        leftStickPosition = movementValue.Get<Vector2>();
+    }
+    private void OnSword()
+    {
+        if (!swordPressed)
+        {
+             m_Animator.SetTrigger("Attack");
+             m_Animator.ResetTrigger("Idal");
+             Debug.Log("Sword");
+             swordPressed = true;
+        }
+        else
+        {
+            Debug.Log("SUnpressed");
+            m_Animator.SetTrigger("Idal");
+            m_Animator.ResetTrigger("Attack");
+            swordPressed = false;
+        }
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnBigSword()
     {
+        if (!bigSwordPressed)
+        {
+            
+            Debug.Log("pressed");
+            bigSwordPressed = true;
+            m_Animator.SetTrigger("Smash");
+
+        }
+        else
+        {
+            Debug.Log("Unpressed");
+            m_Animator.SetTrigger("SmashAttack");
+            bigSwordPressed = false;
+        }
         
     }
 }
