13544f84cbd3d3d61c565f739fa460169180385b
EleanorOfBingen <eleanor.ahlberg@edu.futuregames.nu>
2022-11-30 16:00:13 +0100

    changesAttackWorkTommorrow

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -7,13 +7,28 @@ public class EnemyFollow : MonoBehaviour
 {
     private float distance;
     private float maxDistance = 5f;
+    private bool readyToAttack;
+    private float chargeUpTime = 0.5f;
+    private float chargeUpTimeMax;
+    private float timeAttack = 0.5f;
+    private float timeAttackMax;
+    private float pushForce = 20;
+
+    private Vector3 destinationAttack;
+
+
     public Transform player;
     NavMeshAgent enemyAgent;
     private float timer;
 
     private bool amIHit;
+    
+ 
+
     void Start()
     {
+
+        chargeUpTimeMax = chargeUpTime;
         //Enemy have pathfinding
         enemyAgent = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -22,10 +37,12 @@ public class EnemyFollow : MonoBehaviour
 
     void Update()
     {
+        distance = Vector3.Distance(transform.position, player.transform.position);
         if (amIHit)
         {
+            enemyAgent.isStopped = true;
             timer -= Time.deltaTime;
-            if(timer < 0)
+            if (timer < 0)
             {
 
                 amIHit = false;
@@ -33,12 +50,43 @@ public class EnemyFollow : MonoBehaviour
             }
 
         }
-        else
+
+        if (readyToAttack == false)
         {
-            enemyAgent.destination = player.transform.position;
+ 
+            if (inRange())
+            {
+                destinationAttack = player.transform.position;
+                readyToAttack = true;
+
+
+
+            }
+            else
+            {
+                enemyAgent.isStopped = false;
+                enemyAgent.destination = player.transform.position;
+
+
+            }
+        }
+
+
+        if (readyToAttack)
+        {
+            Vector3 newDest = (destinationAttack - transform.position).normalized;
+            chargeUpTime -= Time.deltaTime;
+            if(chargeUpTime <= 0 && timeAttack <= 0)
+            {
+                transform.Translate(newDest * pushForce * Time.deltaTime);
+                timeAttack -= Time.deltaTime;
+            }
+      
+
 
 
         }
+
         //Ai follow player
         //Debug.Log(player.transform.position);
       
@@ -50,5 +98,14 @@ public class EnemyFollow : MonoBehaviour
         amIHit=true;
 
     }
+
+    private bool inRange()
+    {
+        
+
+        return distance <= maxDistance;
+
+    }
+    
 
 }
